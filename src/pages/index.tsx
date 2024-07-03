cdf79efa86c3da80d170a386ea8112ae86dd2c7f
Sheng-Loong Su <sushengloong@gmail.com>
2024-07-03 22:27:53 +0800

    Hide features

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -35,9 +35,9 @@ export default function Home(): JSX.Element {
       title={`Hello from ${siteConfig.title}`}
       description="Description will go into a meta tag in <head />">
       <HomepageHeader />
-      <main>
-        <HomepageFeatures />
-      </main>
+      {/* <main> */}
+        {/* <HomepageFeatures /> */}
+      {/* </main> */}
     </Layout>
   );
 }
